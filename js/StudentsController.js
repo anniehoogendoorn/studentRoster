b182968c370ed6a2e6aa237d5c74574053a2e802
Annie <anniehoogendoorn@gmail.com>
2015-09-19 21:45:46 -0700

    Add addStudent functionality to html page and StudentsController.

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,8 @@
+// The addStudent method will create a new student object based on the user's input and push it into the students array. It then resets the studentName model to null so that the field in the HTML is cleared
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [
-    { name: "Sam Schmidt" },
-    { name: "Jessica Martin" },
-    { name: "Sandy Smith" },
-    { name: "Ryan Samuels" },
-    { name: "Brentwood Davis" }
-  ]
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
 });
